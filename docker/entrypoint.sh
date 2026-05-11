2d91e6505b5b37db55541dcab552efb0f08a7b5b
b1nchilling <binchilling@tutamail.com>
2026-05-11 14:26:18 +0300

    fixed version of the entrypoint and compose in order to run a PnP lunch

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,11 +6,11 @@ source /opt/ros/galactic/setup.bash
 cd /root/dev/src
 
 echo "[entrypoint] building workspace..."
+
 rm -rf build install log
 
-# colcon build --symlink-install
 colcon build
 
-source ~/dev/src/install/setup.bash
+source install/setup.bash
 
 exec "$@"
