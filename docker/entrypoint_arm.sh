6ee17f7a8a1ac413a74f9cd1553a8423f3edf549
b1nchilling <binchilling@tutamail.com>
2026-05-18 13:01:22 +0300

    if works, final version

--- docker/entrypoint_arm.sh
+++ docker/entrypoint_arm.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 set -e
 
+echo "[entrypoint] sourcing ROS..."
+
 source /opt/ros/galactic/install/setup.bash
 
 cd /root/dev/src
 
-echo "[entrypoint] building workspace..."
+echo "[entrypoint] cleaning workspace..."
 
 rm -rf build install log
 
+echo "[entrypoint] building workspace..."
+
 colcon build
 
-source install/setup.bash
+echo "[entrypoint] sourcing workspace..."
+
+source /root/dev/src/install/setup.bash
+
+echo "[entrypoint] starting command..."
 
 exec "$@"
