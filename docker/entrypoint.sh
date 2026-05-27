337457f1bd24353f4edbafb40bc8a92df8ee25ad
b1nchilling <binchilling@tutamail.com>
2026-05-27 17:45:54 +0300

    made the files cleaner

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,9 +6,7 @@ source /opt/ros/galactic/setup.bash
 cd /root/dev/src
 
 echo "[entrypoint] building workspace..."
-
 rm -rf build install log
-
 colcon build
 
 source install/setup.bash
--- docker/entrypoint_arm.sh
+++ docker/entrypoint_arm.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "[startup] sourcing ROS..."
-
+echo "[startup] sourcing ROS and workspace..."
 source /opt/ros/galactic/install/setup.bash
-
-echo "[startup] sourcing workspace..."
-
 source /root/dev/install/setup.bash
 
 echo "[startup] launching..."
-
 exec ros2 launch ibn_mavlink pixhawk_bridge_launch.py
-
-echo "[startup] done."
